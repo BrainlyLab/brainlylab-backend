d44e59d5b89c6e61bf985c8c48415f54662b5d87
ajruttam <ajruttam@gmail.com>
2026-03-05 21:58:31 +0530

    Initial backend setup

---
+++ config/settings/dev.py
@@ -0,0 +1,11 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:5500',
+    'http://127.0.0.1:5500',
+]
